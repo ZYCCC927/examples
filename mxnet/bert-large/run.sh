c201ae7cb133ec05b33c01fc122108321d0bad2d
ZYCCC927 <61256396+ZYCCC927@users.noreply.github.com>
2021-03-22 14:04:51 +0800

    Update run.sh

--- mxnet/bert-large/run.sh
+++ mxnet/bert-large/run.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
+# Docker Image: zycccc/bsc-mxnet-py3
+git clone -b bytescheduler https://github.com/netx-repo/byteps.git
+bash byteps/bytescheduler/setup_nodes_mxnet_ps.sh
+cd byteps && git clone https://github.com/ZYCCC927/examples.git
+
+
 # install gluon-nlp under this dir
-THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cd $THIS_DIR/gluon-nlp
+sudo su root
+cluster
+cd /home/cluster/byteps/examples/mxnet/bert-large/gluon-nlp
 python3 setup.py install
 
 # prepare dict 
-mkdir -p ~/.mxnet/models
-cd ~/.mxnet/models 
+mkdir -p /home/cluster/.mxnet/models
+cd /home/cluster/.mxnet/models 
 wget https://apache-mxnet.s3-accelerate.dualstack.amazonaws.com/gluon/dataset/vocab/book_corpus_wiki_en_uncased-a6607397.zip
+apt-get install unzip
 unzip *.zip
        
 # below params are used in V100-32GB, with synthetic data
